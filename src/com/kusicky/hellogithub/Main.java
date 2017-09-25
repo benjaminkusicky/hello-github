46a7a032d1a9fa56207c766793c77dde1458434e
benjaminkusicky <benjaminkusicky@gmail.com>
2017-09-25 11:36:42 +0200

    Initial commit

---
+++ src/com/kusicky/hellogithub/Main.java
@@ -0,0 +1,10 @@
+package com.kusicky.hellogithub;
+
+/**
+ * Created by Benjamin on 25.09.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Github");
+    }
+}
